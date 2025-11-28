6787716393085e5577cb2a55072db9d9b21d097e
aaronshinwu <aaronshinwu03@gmail.com>
2025-11-27 21:17:55 -0300

    Add management command to create superuser automatically

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+# DEBES AÑADIR esta importación para poder usar el decorador
+from django.views.decorators.csrf import csrf_exempt 
 from .views import MensajeAPIView, MensajeDetalleAPIView, MensajesPorMotivoView, MensajePublicCreateView
 from .views_admin import AdminTokenObtainPairView
 
 urlpatterns = [
-    path('Mensajes/', MensajePublicCreateView.as_view(), name='mensaje-create'),  # publico
-    path('Mensajes/admin/', MensajeAPIView.as_view(), name='mensaje-list-admin'),  # admin
+    # Aplicar csrf_exempt a la vista de creación de mensajes para permitir POST sin token
+    path('Mensajes/', csrf_exempt(MensajePublicCreateView.as_view()), name='mensaje-create'),
+    
+    path('Mensajes/admin/', MensajeAPIView.as_view(), name='mensaje-list-admin'),
     path('Mensajes/<int:id_mensaje>/', MensajeDetalleAPIView.as_view(), name='mensaje-detalle'),
     path('Mensajes/filtrar/', MensajesPorMotivoView.as_view(), name='mensajes-filtrar'),
     path('token-admin/', AdminTokenObtainPairView.as_view(), name='token_admin'),
